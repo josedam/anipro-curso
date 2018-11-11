4001f5ad28c9406d96183d1ea5b3998713165316
josedam <josedam@yahoo.com>
2018-11-11 17:40:52 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { ProgressComponent } from "./pages/progress/progress.component";
+import { Graficas1Component } from "./pages/graficas1/graficas1.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { SiderbarComponent } from "./shared/siderbar/siderbar.component";
+import { BreadcrumbsComponent } from "./shared/breadcrumbs/breadcrumbs.component";
+
+import { PagenotfoundComponent } from "./shared/pagenotfound/pagenotfound.component";
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SiderbarComponent,
+    BreadcrumbsComponent,
+    PagenotfoundComponent,
+    PagesComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
